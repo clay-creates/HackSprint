24c1a2dc90132a9cba28f04693d444dcf6ca20d8
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-04-13 01:11:55 -0500

    Updated intro Scene and Project Settings
    I added an intro scene that will display the intro storyline to the game in a typewriter format. It is not complete, but the ground work has been laid.
    
    I also adjusted the project settings to allow the user to maximize the screen and the game change size with it, instead of staying the same.

---
+++ IntroDialogue.gd
@@ -0,0 +1,42 @@
+extends Label
+
+# God forgive me for what I've done.
+var full_text: String = "Modern Day. 
+
+<Character Name> Has been a private investigator for years. He's always been ahead of the 
+technology curve, using the latest technology to solve his case. He's good. Too good.
+
+He's an overworker, and unfortunatly has lead him to a lonely life. He had a wife and kids, 
+but she left some time ago because he's too invested in his work. 
+
+The last few months he's been assigned to the biggest case yet. A case that has taken 
+over his entire life, and lead him to drinking to ease the pain and guilt of his past.
+
+Last night, he discovered a huge clue that shook him to his core. To ease himself, 
+he did what he's recently done to ease the pain. He hit the bar. 
+
+This time he had too much..."
+#var visible_characters: int = 0
+#var visible_characters: int = 0
+var text_speed: float = 0.05  # Seconds between characters
+var timer: Timer
+var begin_button: Button
+
+func _ready():
+	text = ""
+	begin_button = get_node("Begin")  # Adjust the path as necessary
+	begin_button.visible = false  # Hide the button initially
+	timer = Timer.new()  # Create a new Timer
+	timer.wait_time = text_speed
+	timer.one_shot = false  # If you want continuous effect without restarting the timer
+	timer.connect("timeout", Callable(self, "_on_Timer_timeout"))  # Connect signal to function
+	add_child(timer)  # Add the Timer to the Label node
+	timer.start()  # Start the timer
+
+func _on_Timer_timeout():
+	if visible_characters < full_text.length():
+		visible_characters += 1
+		text = full_text.substr(0, visible_characters)
+	else:
+		timer.stop()  # Stop the timer when all text has been displayed
+		begin_button.visible = true
--- TypingTextEffect.gd
+++ TypingTextEffect.gd
@@ -1,17 +1,63 @@
-extends Label
+extends Dialogue
 
-var pc: float
-var percent_visible = 0.0
+# The text to display using the typing effect.
+var story_text = "Modern Day.\n
+	\n
+	<Character Name> Has been a private investigator for years. He's always been ahead of the technology curve, using\n
+	the latest technology to solve his case. He's good. Too good.\n
+	\n
+	He's an overworker, and unfortunately has led him to a lonely life. He had a wife and kids, but she left some time ago\n
+	because he's too invested in his work.\n
+	\n
+	The last few months he's been assigned to the biggest case yet. A case that has taken over his entire life, and\n
+	led him to drinking to ease the pain and guilt of his past.\n
+	\n
+	Last night, he discovered a huge clue that shook him to his core. To ease himself, he did what he's recently done to\n
+	ease the pain. He hit the bar. This time he had too much...\n"
+# This function scrolls text with a typewriter effect.
 
+var typing_speed = .1
+var read_time = 2
 
-func make_text(new_text: String):
-	text = new_text
-	percent_visible = 0.0
-	await get_tree().idle_frame
-	pc = 1.0 / text.length()
+var current_message = 0
+var display = ""
+var current_char = 0
 
-func _on_Timer_timeout():
-	percent_visible += pc
-	if percent_visible >= 1.0:
-		$Timer.stop()
-		emit_signal("text_tying_finished")
+func _ready():
+	# Start the typing effect when the node is ready.
+	start_dialogue()
+
+func start_dialogue():
+	current_message = 0
+	display = ""
+	current_char = 0
+	
+	$next_char.set_wait_time(typing_speed)
+	$next_char.start()
+
+func stop_dialogue():
+	queue_free()
+
+func _on_next_char_timeout():
+	if (current_char < len(story_text[current_message])):
+		var next_char = story_text[current_message][current_char]
+		display += next_char
+		
+		$Label.text = display
+		current_char += 1
+	else:
+		$next_char.stop()
+		$next_message.one_shot = true
+		$next_message.set_wait_time(read_time)
+		$next_message.start()
+
+func _on_next_message_timeout():
+	if (current_message == len(story_text) - 1):
+		stop_dialogue()
+	else: 
+		current_message += 1
+		display = ""
+		current_char = 0
+		$next_char.start()
+	
+	
