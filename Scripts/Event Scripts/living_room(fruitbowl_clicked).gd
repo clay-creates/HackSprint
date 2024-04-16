f6feb5b050bfb0001e4c19e50649d1d602c6fff4
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-04-15 19:56:14 -0500

    pull files and push changes, then testing

--- Scripts/Event Scripts/living_room(fruitbowl_clicked).gd
+++ Scripts/Event Scripts/living_room(fruitbowl_clicked).gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var is_note_visible = false
+
 func change_scene(scene_path):
 	get_tree().change_scene_to_file(scene_path)
 
@@ -20,3 +22,18 @@ func _on_left_arrow_input_event(viewport, event, shape_idx):
 		
 		if str(current_scene) == "/root/Living-Room(Fruitbowl-Clicked)":
 			change_scene("res://Scenes/dining_room.tscn")
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if is_note_visible:
+			get_node("Note-R (large)").hide()
+			is_note_visible = false
+		else:
+			get_node("Note-R (large)").show()
+			is_note_visible = true
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if is_note_visible:
+			get_node("Note-R (large)").hide()
+			is_note_visible = false
